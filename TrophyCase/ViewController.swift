bf9a4857f5acfc9cb481b774d310a1ff03983de7
Jake <jake.stephens9@gmail.com>
2020-08-16 22:41:38 -0400

    Initial Commit

---
+++ TrophyCase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrophyCase
+//
+//  Created by Jake on 2020-08-16.
+//  Copyright © 2020 Jake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
